ffb9ce3e96bf726b37b213e87c16c59c29c4b5c4
孙思杰 <443152178@qq.com>
2015-06-27 17:49:51 +0800

    可以保存教师了

--- application/controllers/Evaluate.php
+++ application/controllers/Evaluate.php
@@ -20,6 +20,12 @@ class Evaluate extends CI_Controller{
             echo "您还未登录";
             exit();
         }
-        $this->jiaowu->getCourseArray();
+        $jw = $this->jiaowu;
+        $course = $jw->getCourseArray();
+
+        $t = &$course[3]['teachers'][0];
+        $jw->evaluateTeacher($t);
+        $res = $jw->saveTeacher($t);
+        echo $res;
     }
 }
--- application/models/Jiaowu.php
+++ application/models/Jiaowu.php
@@ -8,8 +8,10 @@
 
 class Jiaowu extends CI_Model{
     private static $url_test_login = "/login";
-    private static $url_test_courses_list = "/xspj/queryPjkc";
-    private static $url_test_teachers_list = "/xspj/pjkc";
+    private static $url_courses_list = "/xspj/queryPjkc";
+    private static $url_teachers_list = "/xspj/pjkc";
+    private static $url_insert_teacher = "/xspj/insertPj";
+    private static $url_update_teacher = "/xspj/updatePj";
     public function testHasLoggedIn()
     {
         $ch = $this->connection->getCurlPointer();
@@ -42,7 +44,7 @@ class Jiaowu extends CI_Model{
 
 
         $ch = $this->connection->getCurlPointer();
-        curl_setopt($ch,CURLOPT_URL,$_SESSION['root'] . self::$url_test_teachers_list);
+        curl_setopt($ch,CURLOPT_URL,$_SESSION['root'] . self::$url_teachers_list);
         curl_setopt($ch,CURLOPT_POST,true);
         curl_setopt($ch,CURLOPT_POSTFIELDS,[
             "skjs" => $course['form']['SKJS'],
@@ -72,7 +74,7 @@ class Jiaowu extends CI_Model{
             $params = explode(',',$params);
             //参数本身室友单引号，我们需要去掉它
             array_walk($params,function(&$item){
-                trim($item,"'");
+                $item = trim($item,"'");
             });
 
             //function pjjs(zgh,yxfldm,fldm,sfpj,kcdm,jxbh){
@@ -86,7 +88,7 @@ class Jiaowu extends CI_Model{
                 ) = $params;
 
             //自定义字段
-            $t['name'] = $names[$index];
+            $t['skjs'] = $names[$index];
 
             $course['teachers'][] = $t;
         }
@@ -96,7 +98,7 @@ class Jiaowu extends CI_Model{
         if($this->courses != null)return $this->courses;
 
         $ch = $this->connection->getCurlPointer();
-        curl_setopt($ch,CURLOPT_URL,$_SESSION['root'] . self::$url_test_courses_list);
+        curl_setopt($ch,CURLOPT_URL,$_SESSION['root'] . self::$url_courses_list);
         $course_html = Connection::getResponsBody($ch);
 
         //一个都没有匹配到，不会吧，很可能是错误了，直接展示错误截面把= =
@@ -128,5 +130,93 @@ class Jiaowu extends CI_Model{
             $this->courses[]  = $course;
         }
         return $this->courses;
+    }
+
+    //评价一个教师
+    public function evaluateTeacher(&$teacher,array $pj = array())
+    {
+        //前6个是选择，ABC分别为123，如果没有填则默认为A
+        //需要检查是否有全部相同的,如果全部相等，那么array_unique自然为空
+
+        //评价的表单
+        //重置掉，保证后面合并array的时候这个是一维数组
+        unset($teacher['pjform']);
+
+        $testUnique = array();
+        for($i = 0;$i < 6; $i ++)
+        {
+            //转换失败就是0，没影响。
+            if(!isset($pj[$i]))
+            {
+                $testUnique[$i] = $pj[$i] = 1;
+            }
+            else
+            {
+                $pj[$i] = intval($pj[$i]);
+                if(!in_array($pj[$i],[1,2,3]))
+                {
+                    $pj[$i] = 1;
+                }
+                $testUnique[$i] = $pj[$i];
+            }
+        }
+
+        //全部一样，你在逗我？。
+        $unique = array_unique($testUnique);
+        if(count($unique) == 1)
+        {
+            $pj[0] = $pj[0] == 1 ? 2: 1;
+        }
+
+        //评语
+        if(!isset($pj[6]))$pj[6] = "您的课让人受益匪浅！";
+
+        //最后填充好的表单
+        $resform = "";
+        //zhi:"02"
+        //ids:"{"zbdm":"0104","zbfz":"02"}"
+        //按上面的格式格式化pj数组
+        $resform .= "pyxx=" . urlencode($pj[6]);
+        foreach($pj as $i => $v)
+        {
+            //注意，教务网是从1开始编号的
+            $i++;
+            if($i > 6)break;//前6个是选项
+            $resform .= "&";
+            $resform .= "zhi=" . urlencode("0$v");
+            $resform .= "&";
+            $resform .= "ids=". urlencode("{\"zbdm\":\"010$i\",\"zbfz\":\"0$v\"}");
+        }
+
+        //开始填写提交表单
+        array_walk($teacher,function(&$item,$key) use (&$resform)
+        {
+            $resform .= "&";
+            $tmp = urlencode($item);
+            $resform .= "$key=$tmp";
+        });
+
+        $resform .= "&gjz=&gjz2=";
+        $teacher['pjform'] = $resform;
+    }
+
+
+    //保存所有教师，但是不保存课程，因为一旦保存课程就没法改了0 0
+    public function saveTeacher($teacher)
+    {
+        if(!isset($teacher['pjform']))//还没有评价，保存个毛线
+        {
+            return false;
+        }
+
+        $ch = $this->connection->getCurlPointer();
+        //已经评价过就是update，否则就是insert
+        $url = $teacher["sfpj"] === "1"?
+            self::$url_update_teacher:self::$url_insert_teacher;
+        curl_setopt($ch,CURLOPT_URL,$_SESSION['root'] . $url);
+        curl_setopt($ch,CURLOPT_POST,true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$teacher['pjform']);
+
+        return curl_exec($ch);
     }
 }
